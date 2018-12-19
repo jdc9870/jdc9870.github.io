399b39df14cf2529489826e34ab108638b2c5dfe
Jason <jdchau@ucsd.edu>
2018-12-18 20:29:37 -0800

    YEEET

--- js/script.js
+++ js/script.js
@@ -18,30 +18,51 @@ var pos;
 var iTextPos = 0; // initialise text position
 var sContents = ''; // initialise contents variable
 var iRow; // initialise current row
-var rip = document.getElementById("rip");
+var aLength = aText.length;
+var i;
+var j;
+var count = 0;
+for (i = 0; i < aLength; i++) {
+    for (j = 0; j < aText[i].length; j++) {
+        count++;
+    }
+}
+var totalCount = (aLength*500)+(iSpeed*count);
 
+function yeet() {
+    var typedtext = document.getElementById("typedtext");
+    var newNode = document.createElement("div");
+    newNode.innerHTML = output[3];
+    typedtext.parentNode.insertBefore(newNode, typedtext.nextSibling);
+}
+
+function fkdis() {
+    huehue = setTimeout(function() {yeet();}, totalCount);
+}
 function typewriter()
 {
- sContents = ' ';
- iRow = 0;
- pos = 0;
- var typedtext = document.getElementById("typedtext");
+    sContents = ' ';
+    iRow = 0;
+    pos = 0;
+    var typedtext = document.getElementById("typedtext");
  
- while ( iRow < iIndex ) {
-  sContents += aText[iRow++] + '<br/>' + " " + output[pos++] + '<br/>' + '<br/>';
- }   
-    
- typedtext.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "_";
-    
- if ( iTextPos++ == iCurrentLength ) {
-  iTextPos = 0;
-  iIndex++;
-  if ( iIndex != aText.length ) {
-   iCurrentLength = aText[iIndex].length;  
-   setTimeout("typewriter()", 500);  
-  }
- } else {
-  setTimeout("typewriter()", iSpeed);
- } 
+    while ( iRow < iIndex ) {
+        sContents += aText[iRow++] + '<br/>' + " " + output[pos++] + '<br/>' + '<br/>';
+    }   
+ 
+    typedtext.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "_";
+     
+    if ( iTextPos++ == iCurrentLength ) {
+        iTextPos = 0;
+        iIndex++;
+        if ( iIndex != aText.length ) {
+            iCurrentLength = aText[iIndex].length;  
+            setTimeout("typewriter()", 500);
+        }   
+    } 
+    else {   
+        setTimeout("typewriter()", iSpeed);   
+    }   
 }
 typewriter();
+fkdis();
